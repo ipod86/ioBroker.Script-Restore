2ca652731222b0dfee12e5b39f9a4e9a015c873f
ipod86 <david@graef.email>
2026-03-14 20:46:01 +0100

    Initial commit: Fixed adapter name and boilerplate

---
+++ admin/words.js
@@ -0,0 +1,46 @@
+/* eslint no-unused-vars: off */
+/* eslint no-global-assign: off */
+/* global systemDictionary */
+'use strict';
+
+systemDictionary = {
+	'script-restore adapter settings': {
+		'en': 'Adapter settings for script-restore',
+		'de': 'Adaptereinstellungen für script-restore',
+		'ru': 'Настройки адаптера для script-restore',
+		'pt': 'Configurações do adaptador para script-restore',
+		'nl': 'Adapterinstellingen voor script-restore',
+		'fr': "Paramètres d'adaptateur pour script-restore",
+		'it': "Impostazioni dell'adattatore per script-restore",
+		'es': 'Ajustes del adaptador para script-restore',
+		'pl': 'Ustawienia adaptera dla script-restore',
+		'uk': 'Налаштування адаптера для script-restore',
+		'zh-cn': 'script-restore的适配器设置'
+	},
+	'option1': {
+		'en': 'option1',
+		'de': 'Option1',
+		'ru': 'вариант 1',
+		'pt': 'opção1',
+		'nl': 'optie1',
+		'fr': 'option1',
+		'it': 'opzione1',
+		'es': 'opción1',
+		'pl': 'opcja 1',
+		'uk': 'варіант1',
+		'zh-cn': '选项1'
+	},
+	'option2': {
+		'en': 'option2',
+		'de': 'Option2',
+		'ru': 'вариант 2',
+		'pt': 'opção2',
+		'nl': 'optie2',
+		'fr': 'option2',
+		'it': 'opzione2',
+		'es': 'opción2',
+		'pl': 'opcja 2',
+		'uk': 'варіант2',
+		'zh-cn': '选项2'
+	}
+};
